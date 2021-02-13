684f670e198b7ea8b9668596b8cbc35a20a861b1
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-02-13 17:49:56 +0200

    Prepping code ready for coming components
    Cannot really do any real logic yet, no hardware

--- arduino/Gripper/Gripper.ino
+++ arduino/Gripper/Gripper.ino
@@ -13,17 +13,28 @@ int motorA1 = 2;
 int motorA2 = 3;
 int motorB1 = 4;
 int motorB2 = 5;
+int endStop = 6;
+int robotInput = 7;
+int robotOutput = 8;
 
 // Variables
+boolean homingDone = false;
 const int stepsPerRevolution = 200;   // steps per revolution
 const int stepperMotorSpeed = 300;    // stepper motor speed
 Stepper myStepper(stepsPerRevolution, motorA1, motorA2, motorB1, motorB2);
+int currentStepPosition = 0;
 
 
 // SETUP
 void setup() {
   // Init serial
   Serial.begin(9600);
+  // EStop
+  pinMode(endStop, INPUT_PULLUP);
+  // NX100 input
+  pinMode(robotInput, INPUT_PULLUP);
+  // NX100 output
+  pinMode(robotOutput, OUTPUT);
   // Init stepper
   myStepper.setSpeed(stepperMotorSpeed);
   Serial.println("Setup ok");
@@ -32,23 +43,54 @@ void setup() {
 
 // LOOP
 void loop() {
-  delay(1 * 1000);
-
+  if (!homingDone) {
+    // homing();
+  }
+  
+  delay(1000);
   myStepper.step(2500);
 
+
+  // Todo, robot gives gripper close position value command, 
+  // move gripper to wanted position and set output HIGH to acknowledge
   /*
-  if (getValue(commandToProcess,';',0) == "forward") {
-    myStepper.step( getValue(commandToProcess,';',1).toInt() );
-  } else if (getValue(commandToProcess,';',0) == "backward") {
-    myStepper.step(-getValue(commandToProcess,';',1).toInt() );
-  }
+  int commandPositionValue = digitalRead(robotInput);
+  ...
+  digitalWrite(robotOutput, HIGH);
+  */
+
+  // Todo, robot gives gripper open position value command,
+  // move gripper to wanted position and set output LOW to acknowledge
+  /*
+  int commandPositionValue = digitalRead(robotInput);
+  ...
+  digitalWrite(robotOutput, LOW);
   */
   
   turnOffStepper();
 }
 
 
-// Solves stepper driver overheating problem
+/**
+ * Homing procedure
+ * 1. close gripper more till hit estop
+ * 2. open gripper fully
+ * 3. set homing done
+ */
+void homing() {
+  int eStopVal = digitalRead(endStop);
+  while (eStopVal == LOW) {
+    myStepper.step(1);
+  }
+  myStepper.step(-100); // open fully
+  homingDone = true;
+}
+
+
+/**
+ * Turn off stepper driver
+ * solves stepper driver overheating problem
+ */
 void turnOffStepper() {
   digitalWrite(motorA1, LOW);
   digitalWrite(motorA2, LOW);
