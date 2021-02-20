efa5461ffb4a756613a7dd3673449cf9fff38fd7
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-02-20 19:18:13 +0200

    Working gripper signal, homing program for Arduino
    Don't have actual hardware gripper yet to try it out

--- arduino/Gripper/Gripper.ino
+++ arduino/Gripper/Gripper.ino
@@ -21,6 +21,7 @@ int robotOutput = 8;
 boolean homingDone = false;
 const int stepsPerRevolution = 200;   // steps per revolution
 const int stepperMotorSpeed = 300;    // stepper motor speed
+const int gripperStepsFullyOpen = -5000; // how many steps needed to reverse when gripper is fully open
 Stepper myStepper(stepsPerRevolution, motorA1, motorA2, motorB1, motorB2);
 int currentStepPosition = 0;
 
@@ -35,6 +36,7 @@ void setup() {
   pinMode(robotInput, INPUT_PULLUP);
   // NX100 output
   pinMode(robotOutput, OUTPUT);
+  digitalWrite(robotOutput, LOW); // off
   // Init stepper
   myStepper.setSpeed(stepperMotorSpeed);
   Serial.println("Setup ok");
@@ -44,31 +46,32 @@ void setup() {
 // LOOP
 void loop() {
   if (!homingDone) {
-    // homing();
+    homing();
   }
 
+
+  // Robot gives gripper close position value command, finally acknowledge with output signal
   int rInputVal = digitalRead(robotInput);
   if (rInputVal == LOW) {
-    // delay(1000);
-    myStepper.step(2500);
+    // closing gripper
+    int newPos = abs(gripperStepsFullyOpen);
+    if (currentStepPosition < newPos) {
+      Serial.println("Closing gripper");
+      myStepper.step(newPos);
+      currentStepPosition = newPos;
+      digitalWrite(robotOutput, HIGH); // acknowledge closed
+    }
+  } else {
+    // opening gripper
+    if (currentStepPosition > gripperStepsFullyOpen) {
+      Serial.println("Opening gripper");
+      myStepper.step(gripperStepsFullyOpen);
+      currentStepPosition = gripperStepsFullyOpen;
+      digitalWrite(robotOutput, LOW); // acknowledge open
+    }
   }
 
-  // Todo, robot gives gripper close position value command, 
-  // move gripper to wanted position and set output HIGH to acknowledge
-  /*
-  int commandPositionValue = digitalRead(robotInput);
-  ...
-  digitalWrite(robotOutput, HIGH);
-  */
 
-  // Todo, robot gives gripper open position value command,
-  // move gripper to wanted position and set output LOW to acknowledge
-  /*
-  int commandPositionValue = digitalRead(robotInput);
-  ...
-  digitalWrite(robotOutput, LOW);
-  */
-  
   turnOffStepper();
 }
 
@@ -80,11 +83,20 @@ void loop() {
  * 3. set homing done
  */
 void homing() {
-  int eStopVal = digitalRead(endStop);
-  while (eStopVal == LOW) {
+  Serial.println("Homing gripper...");
+  int steps = 0;
+  int eStopVal = HIGH;
+  while (eStopVal == HIGH) {
+    eStopVal = digitalRead(endStop);
     myStepper.step(1);
+    steps++;
   }
-  myStepper.step(-100); // open fully
+  Serial.print("Gripper endstop hit at ");
+  Serial.print(steps);
+  Serial.println(" steps.. gripper is fully closed");
+  myStepper.step(gripperStepsFullyOpen); // open fully
+  currentStepPosition = gripperStepsFullyOpen;
+  Serial.println("Gripper fully open, saving current position");
   homingDone = true;
 }
 
