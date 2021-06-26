f149eed9542fea1780d63970f54a453221131cb4
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-06-26 15:34:55 +0300

    Change project structure for proper build

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(
     name='nx100_remote_control',
     packages=[
+        'nx100_remote_control',
         'nx100_remote_control.module',
         'nx100_remote_control.objects'
     ],
-    package_dir={'': 'nx100_remote_control'},
+    package_dir={'': 'src'},
     include_package_data=True,
 )
