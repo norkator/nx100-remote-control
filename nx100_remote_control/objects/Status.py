b0f4356d58da275967ba9d737f068200c7cb28c3
Martin Kankaanranta <55850510+norkator@users.noreply.github.com>
2021-06-26 13:36:44 +0300

    Fix lint problems

--- nx100_remote_control/objects/Status.py
+++ nx100_remote_control/objects/Status.py
@@ -7,56 +7,56 @@ class Status(object):
 
     def is_command_remote(self):
         p = 0
-        return (self.data_1[p] is '1') if len(self.data_1) > p else False
+        return (self.data_1[p] == '1') if len(self.data_1) > p else False
 
     def is_play(self):
         p = 1
-        return (self.data_1[p] is '1') if len(self.data_1) > p else False
+        return (self.data_1[p] == '1') if len(self.data_1) > p else False
 
     def is_teach(self):
         p = 2
-        return (self.data_1[p] is '1') if len(self.data_1) > p else False
+        return (self.data_1[p] == '1') if len(self.data_1) > p else False
 
     def is_safety_speed_operation(self):
         p = 3
-        return (self.data_1[p] is '1') if len(self.data_1) > p else False
+        return (self.data_1[p] == '1') if len(self.data_1) > p else False
 
     def is_running(self):
         p = 4
-        return (self.data_1[p] is '1') if len(self.data_1) > p else False
+        return (self.data_1[p] == '1') if len(self.data_1) > p else False
 
     def is_auto(self):
         p = 5
-        return (self.data_1[p] is '1') if len(self.data_1) > p else False
+        return (self.data_1[p] == '1') if len(self.data_1) > p else False
 
     def is_one_cycle(self):
         p = 6
-        return (self.data_1[p] is '1') if len(self.data_1) > p else False
+        return (self.data_1[p] == '1') if len(self.data_1) > p else False
 
     def is_step(self):
         p = 7
-        return (self.data_1[p] is '1') if len(self.data_1) > p else False
+        return (self.data_1[p] == '1') if len(self.data_1) > p else False
 
     def is_servo_on(self):
         p = 1
-        return (self.data_2[p] is '1') if len(self.data_2) > p else False
+        return (self.data_2[p] == '1') if len(self.data_2) > p else False
 
     def is_error_occurring(self):
         p = 2
-        return (self.data_2[p] is '1') if len(self.data_2) > p else False
+        return (self.data_2[p] == '1') if len(self.data_2) > p else False
 
     def is_alarm_occurring(self):
         p = 3
-        return (self.data_2[p] is '1') if len(self.data_2) > p else False
+        return (self.data_2[p] == '1') if len(self.data_2) > p else False
 
     def is_command_hold(self):
         p = 4
-        return (self.data_2[p] is '1') if len(self.data_2) > p else False
+        return (self.data_2[p] == '1') if len(self.data_2) > p else False
 
     def is_external_hold(self):
         p = 5
-        return (self.data_2[p] is '1') if len(self.data_2) > p else False
+        return (self.data_2[p] == '1') if len(self.data_2) > p else False
 
     def is_programming_pendant_hold(self):
         p = 6
-        return (self.data_2[p] is '1') if len(self.data_2) > p else False
+        return (self.data_2[p] == '1') if len(self.data_2) > p else False
